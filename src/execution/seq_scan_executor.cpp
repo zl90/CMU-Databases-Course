d8084c7ab8da2c9194f0fd60ccec43a5301e3341
zl90 <mb541@protonmail.com>
2024-04-27 22:11:32 +1000

    Partially implement SeqScan executor

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -10,14 +10,31 @@
 //
 //===----------------------------------------------------------------------===//
 
+// make -j$(nproc) sqllogictest && ./bin/bustub-sqllogictest ../test/sql/p3.00-primer.slt --verbose
+
 #include "execution/executors/seq_scan_executor.h"
+#include "catalog/catalog.h"
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan)
+    : AbstractExecutor(exec_ctx), plan_(plan) {}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() {}
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  const auto &oid = plan_->GetTableOid();
+  const auto &catalog = exec_ctx_->GetCatalog();
+  const auto &table_info = catalog->GetTable(oid);
+  const auto &table = table_info->table_.get();
+
+  std::vector<Value> values;
+  values.reserve(GetOutputSchema().GetColumnCount());
+
+  // Figure out how to get the correct row from the table. May need to track
+  // the latest accessed tuple RID? Then I can get the next one after that.
+
+  return false;
+}
 
 }  // namespace bustub
