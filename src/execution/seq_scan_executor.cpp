026794948800391be5e18ba7a8c3bf18c586b32d
zl90 <mb541@protonmail.com>
2024-04-28 22:10:10 +1000

    Implement SeqScan executor

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -13,28 +13,33 @@
 // make -j$(nproc) sqllogictest && ./bin/bustub-sqllogictest ../test/sql/p3.00-primer.slt --verbose
 
 #include "execution/executors/seq_scan_executor.h"
+#include <memory>
 #include "catalog/catalog.h"
 
 namespace bustub {
 
 SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan)
-    : AbstractExecutor(exec_ctx), plan_(plan) {}
-
-void SeqScanExecutor::Init() {}
-
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+    : AbstractExecutor(exec_ctx), plan_(plan) {
   const auto &oid = plan_->GetTableOid();
   const auto &catalog = exec_ctx_->GetCatalog();
   const auto &table_info = catalog->GetTable(oid);
   const auto &table = table_info->table_.get();
+  table_iterator_ = std::make_unique<TableIterator>(table->MakeIterator());
+}
 
-  std::vector<Value> values;
-  values.reserve(GetOutputSchema().GetColumnCount());
+void SeqScanExecutor::Init() {}
 
-  // Figure out how to get the correct row from the table. May need to track
-  // the latest accessed tuple RID? Then I can get the next one after that.
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  if (table_iterator_->IsEnd()) {
+    return false;
+  }
 
-  return false;
+  *tuple = table_iterator_->GetTuple().second;
+  *rid = table_iterator_->GetRID();
+
+  ++(*table_iterator_);
+
+  return true;
 }
 
 }  // namespace bustub
