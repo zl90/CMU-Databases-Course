178b019cc2c2ed2041a36e99594eb13be109db48
zl90 <mb541@protonmail.com>
2024-04-29 23:15:32 +1000

    Implement INSERT executor

--- src/execution/insert_executor.cpp
+++ src/execution/insert_executor.cpp
@@ -11,6 +11,7 @@
 //===----------------------------------------------------------------------===//
 
 #include <memory>
+#include "common/config.h"
 
 #include "execution/executors/insert_executor.h"
 
@@ -18,10 +19,51 @@ namespace bustub {
 
 InsertExecutor::InsertExecutor(ExecutorContext *exec_ctx, const InsertPlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {
+  const auto &oid = plan_->GetTableOid();
+  const auto &catalog = exec_ctx_->GetCatalog();
+  catalog_ = catalog;
+  table_info_ = catalog->GetTable(oid);
+}
 
-void InsertExecutor::Init() { throw NotImplementedException("InsertExecutor is not implemented"); }
+void InsertExecutor::Init() { child_executor_->Init(); }
 
-auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool { return false; }
+auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
+  // Get the values from the child node
+  std::vector<Tuple> tuples_to_insert;
+
+  Tuple child_tuple{};
+  while (child_executor_->Next(&child_tuple, rid)) {
+    tuples_to_insert.push_back(child_tuple);
+  }
+
+  // Insert the values into the table itself
+  for (const auto &tuple : tuples_to_insert) {
+    auto tuple_meta = TupleMeta{0, false};
+    table_info_->table_->InsertTuple(tuple_meta, tuple);
+  }
+
+  auto indexes = catalog_->GetTableIndexes(table_info_->name_);
+
+  // Insert the values into the indexes
+  for (const auto &index_info : indexes) {
+    auto index = index_info->index_.get();
+
+    for (auto tuple : tuples_to_insert) {
+      index->InsertEntry(
+          tuple.KeyFromTuple(table_info_->schema_, index_info->key_schema_, index_info->index_->GetKeyAttrs()),
+          tuple.GetRid(), nullptr);
+    }
+  }
+
+  std::vector<Value> values{};
+  values.reserve(GetOutputSchema().GetColumnCount());
+  values.emplace_back(INTEGER, static_cast<int>(tuples_to_insert.size()));
+
+  // Return a tuple containing the number of rows inserted
+  *tuple = Tuple{values, &GetOutputSchema()};
+
+  return !tuples_to_insert.empty();
+}
 
 }  // namespace bustub
--- src/include/execution/executors/insert_executor.h
+++ src/include/execution/executors/insert_executor.h
@@ -15,6 +15,8 @@
 #include <memory>
 #include <utility>
 
+#include "catalog/catalog.h"
+#include "catalog/schema.h"
 #include "execution/executor_context.h"
 #include "execution/executors/abstract_executor.h"
 #include "execution/plans/insert_plan.h"
@@ -57,6 +59,10 @@ class InsertExecutor : public AbstractExecutor {
  private:
   /** The insert plan node to be executed*/
   const InsertPlanNode *plan_;
+
+  std::unique_ptr<AbstractExecutor> child_executor_;
+  TableInfo *table_info_;
+  Catalog *catalog_;
 };
 
 }  // namespace bustub
